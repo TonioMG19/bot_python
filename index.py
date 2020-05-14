fdda8f23bf93511c0ba5ba9d3e7ac5b9c14cbc6d
TonioMG19 <toniokuhn2@gmail.com>
2020-05-14 10:40:37 +0200

    r

--- index.py
+++ index.py
@@ -3,12 +3,17 @@ import asyncio
 
 client = discord.Client()
 
+prefix = '!'
+
 @client.event
 async def on_ready():
 	print("Capitaine Salamèche à l'écoute !")
 
 @client.event
 async def on_message(message):
-	print(message.content)
+	if message.content.startswith(prefix+"hello"):
+		await message.channel.send("Bonjour humain !")
+	elif message.content.startswith(prefix+"carapuce"):
+		await message.channel.send("Beurkkkk")
 
-client.run("NzEwNDA2MDAwMDk1NjU4MDQ4.Xr0AYg.UxYrg8ji4Mb4pnlVgqf6WcBtoIU")
+client.run("TOKEN")
